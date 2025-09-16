a427b20040ed2a5e4d61ddaf71330eb49ef7123f
emanu <emanueli300501@edu.unipar.br>
2025-09-15 22:24:00 -0300

    Trabalho04

--- src/Atividade02.java
+++ src/Atividade02.java
@@ -4,13 +4,34 @@ import java.util.Scanner;
 public class Atividade02 {
     public static void main(String[] args) {
 
-        int[] numero = new int[15];
-        Random ler = new Random(System.in);
+        int[] numeros = new int[15];
+        Random random = new Random();
 
-        for (int i = 0; i < numero.length; i++){
-            numero[i] = ler.nextInt(100)+1;
 
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = random.nextInt(100) + 1;
         }
 
+
+        System.out.println("Números gerados:");
+        for (int n : numeros) {
+            System.out.print(n + " ");
+        }
+
+
+
+        System.out.println("Números pares:");
+        for (int n : numeros) {
+            if (n % 2 == 0) {
+                System.out.print(n + " ");
+            }
+        }
+
+        System.out.println("Números ímpares:");
+        for (int n : numeros) {
+            if (n % 2 != 0) {
+                System.out.print(n + " ");
+            }
+        }
     }
 }
