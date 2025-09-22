1718713490cede3daa1421957c9761a003e7b4bc
emanu <emanueli300501@edu.unipar.br>
2025-09-22 19:22:07 -0300

    Trabalho04

--- src/Atividade05.java
+++ src/Atividade05.java
@@ -2,36 +2,59 @@ import java.util.Scanner;
 
 public class Atividade05 {
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int[][] matriz = new int[3][3];
-            System.out.println("Digite os valores da matriz: ");
-            for (int linha = 0; linha < 3; linha++) {
-                for (int coluna = 0; coluna < 3; coluna++) {
-                    System.out.print("Elemento " + linha + "[" + coluna + "]: ");
-                    matriz[linha][coluna] = sc.nextInt();
-                }
-            }
-            System.out.println("Matriz 3x3:");
-            for (int linha = 0; linha < 3; linha++) {
-                for (int coluna = 0; coluna < 3; coluna++) {
-                    System.out.print(matriz[linha][coluna] + "\t");
-
-                }
-                System.out.println();
-            }
-
-                int soma = 0;
-                for (int[] linhaMatriz : matriz) {
-                    for (int valor : linhaMatriz) {
-                        soma = valor;
-                    }
-                }
-                System.out.println("Soma da Matriz 3x3: "+soma);;
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matriz = new int[3][3];
+        System.out.println("Digite os valores da matriz: ");
+        for (int linha = 0; linha < 3; linha++) {
+            for (int coluna = 0; coluna < 3; coluna++) {
+                System.out.print("Elemento " + linha + "[" + coluna + "]: ");
+                matriz[linha][coluna] = sc.nextInt();
             }
         }
 
+        mostrarMatriz(matriz);
+        somarMatriz(matriz);
+
+    }
+
+    public static void mostrarMatriz(int[][] matriz) {
+
+        System.out.println("Matriz 3x3:");
+        for (int linha = 0; linha < 3; linha++) {
+            for (int coluna = 0; coluna < 3; coluna++) {
+                System.out.print(matriz[linha][coluna] + "\t");
+
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void somarMatriz(int[][] matriz) {
+
+        for (int linha = 0; linha < 3; linha++) {
+
+            int soma = 0;
+
+            for (int coluna = 0; coluna < 3; coluna++) {
+                soma =  soma + matriz[linha][coluna];
+            }
+
+            System.out.println("Soma da linha " + linha + " é " + soma);
+
+        }
+
+    }
+
+}
 
 
 
+/*
+
+   00 01 02
+   10 11 12
+   20 21 22
+
+ */
