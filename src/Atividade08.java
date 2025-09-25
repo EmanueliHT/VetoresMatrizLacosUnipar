29d9bcf4e3eb8503c452f20e979683d02d07478c
emanu <emanueli300501@edu.unipar.br>
2025-09-25 17:09:30 -0300

    Trabalho04

--- src/Atividade08.java
+++ src/Atividade08.java
@@ -1,2 +1,54 @@
+import java.util.Scanner;
+
 public class Atividade08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[][] tabuleiro = new char[3][3];
+
+
+        for (int linha = 0; linha < 3; linha++) {
+            for (int coluna = 0; coluna < 3; coluna++) {
+                tabuleiro[linha][coluna] = ' ';
+            }
+        }
+
+        boolean jogador = true;
+
+
+        System.out.println("Tabuleiro Inicial:");
+        for (int linha = 0; linha < 3; linha++) {
+            for (int coluna = 0; coluna < 3; coluna++) {
+                System.out.print("[" + tabuleiro[linha][coluna] + "]");
+            }
+            System.out.println();
+        }
+
+
+        System.out.print("Linha (0-2): ");
+        int linha = sc.nextInt();
+        System.out.print("Coluna (0-2): ");
+        int coluna = sc.nextInt();
+
+        if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2 || tabuleiro[linha][coluna] != ' ') {
+            System.out.println("Posição inválida! Você perde a vez.");
+        } else {
+            tabuleiro[linha][coluna] = jogador ? 'X' : 'O';
+            jogador = !jogador;
+        }
+
+
+        System.out.println("Tabuleiro Final:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("[" + tabuleiro[i][j] + "]");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Fim de jogo!");
+        sc.close();
+    }
 }
+
+
+
--- src/Atividade09.java
+++ src/Atividade09.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
 public class Atividade09 {
-}
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            double[][] notas = new double[5][2];
+            double[] medias = new double[5];
+
+
+            for (int linha = 0; linha < 5; linha++) {
+                System.out.println("Aluno " + (linha + 1));
+                for (int coluna = 0; coluna < 2; coluna++) {
+                    System.out.print("Nota " + (coluna + 1) + ": ");
+                    notas[linha][coluna] = sc.nextDouble();
+                }
+                medias[linha] = (notas[linha][0] + notas[linha][1]) / 2;
+            }
+
+
+            System.out.println("Médias dos alunos:");
+            for (int linha = 0; linha < 5; linha++) {
+                System.out.println("Aluno " + (linha + 1) + ": " + medias[linha]);
+            }
+
+            sc.close();
+        }
+    }
+
+
